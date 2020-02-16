bbbf94926c758b02747131fdfa3e06afe07fe839
jricafor <jricafor@e3r11p23.42.fr>
2020-02-16 17:12:48 +0100

    rush00

---
+++ rush00/ex00/rush04.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jricafor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/08 19:07:46 by jricafor          #+#    #+#             */
+/*   Updated: 2020/02/08 19:10:16 by jricafor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	head(int x, int y)
+{
+	int m;
+
+	m = 0;
+	if (x > 0 && y > 0)
+	{
+		ft_putchar('A');
+	}
+	while (m < x - 2)
+	{
+		ft_putchar('B');
+		m++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('C');
+	}
+	ft_putchar('\n');
+}
+
+void	middle(int x, int y)
+{
+	int a;
+
+	a = 0;
+	if (x > 0 && y > 0)
+	{
+		ft_putchar('B');
+	}
+	while (a < x - 2)
+	{
+		ft_putchar(' ');
+		a++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('B');
+	}
+	ft_putchar('\n');
+}
+
+void	bottom(int x, int y)
+{
+	int m;
+
+	m = 0;
+	if (x > 0 && y > 0)
+	{
+		ft_putchar('C');
+	}
+	while (m < x - 2)
+	{
+		ft_putchar('B');
+		m++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('A');
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int v;
+
+	v = 0;
+	head(x, y);
+	while (v < y - 2)
+	{
+		middle(x, y);
+		v++;
+	}
+	if (y > 1)
+		bottom(x, y);
+}
