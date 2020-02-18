07acffbbcb06599fffbc5e177d4a71f908f9546f
jricafor <jricafor@e3r11p11.42.fr>
2020-02-18 16:21:08 +0100

    go

---
+++ C/M03/C03_7/ex05/ft_strlcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jricafor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/17 19:56:37 by jricafor          #+#    #+#             */
+/*   Updated: 2020/02/18 10:59:12 by jricafor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int				ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	y;
+	unsigned int	dests;
+	unsigned int	srcs;
+
+	i = 0;
+	y = 0;
+	dests = ft_strlen(dest);
+	while (dest[i] != '\0')
+		i++;
+	if (src[0] == '\0')
+		return (dests);
+	srcs = ft_strlen(src);
+	while (src[y] != '\0' && i < size - 1)
+	{
+		dest[i] = src[y];
+		y++;
+		i++;
+	}
+	return (dests + srcs);
+}
