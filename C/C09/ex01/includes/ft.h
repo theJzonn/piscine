7b8393e9681ecbe71efb0413163137971784703b
jricafor <jricafor@e3r11p11.42.fr>
2020-02-20 21:14:35 +0100

    C09

--- C/C09/ex01/includes/ft.h
+++ C/C09/ex01/includes/ft.h
@@ -1,18 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jricafor <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/20 16:59:44 by jricafor          #+#    #+#             */
-/*   Updated: 2020/02/20 17:00:49 by jricafor         ###   ########.fr       */
+/*   Created: 2020/02/17 12:32:29 by jricafor          #+#    #+#             */
+/*   Updated: 2020/02/17 12:57:42 by jricafor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#ifndef FT_H
+# define FT_H
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
