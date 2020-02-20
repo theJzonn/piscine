5674f2eaa6354e46224150117ec79f50778fd523
theJzonn <jzonnmail@gmail.com>
2020-02-20 22:43:23 +0100

    Update ft_boolean.h

--- C/C08/ex01/ft_boolean.h
+++ C/C08/ex01/ft_boolean.h
@@ -16,9 +16,9 @@
 # define TRUE 1
 # define FALSE 0
 # define SUCCESS 0
-# define EVEN(x) x % 2 != 0
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define EVEN(x) x % 2 == 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 
 typedef int	t_bool;
 
