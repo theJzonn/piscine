f92ef03e30cefac697a07d72bf71c13697158711
theJzonn <jzonnmail@gmail.com>
2020-02-20 22:55:45 +0100

    Update ft_strs_to_tab.c

--- C/C08/ex04/ft_strs_to_tab.c
+++ C/C08/ex04/ft_strs_to_tab.c
@@ -30,11 +30,8 @@ char					*ft_strdup(char *src)
 	char	*str;
 
 	i = 0;
-	while (src[i] != '\0')
-		i++;
-	size = i;
+	size = ft_strlen(src);
 	str = malloc(sizeof(*str) * (size + 1));
-	i = 0;
 	while (i < size)
 	{
 		str[i] = src[i];
@@ -50,20 +47,15 @@ struct s_stock_str		*ft_strs_to_tab(int ac, char **av)
 	t_stock_str		*tab;
 
 	i = 0;
-	if (ac < 1)
-		tab = NULL;
-	else
+	if (!(tab = malloc(sizeof(t_stock_par) * (ac + 1))))
+		return (0);
+	while (i < ac)
 	{
-		tab = malloc(sizeof(t_stock_str) * (ac + 2));
-		while (i < ac)
-		{
-			tab[i].size = ft_strlen(av[i]);
-			tab[i].str = av[i];
-			tab[i].copy = ft_strdup(av[i]);
-			i++;
-		}
-		tab[i].str = 0;
-		tab[i].copy = 0;
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i]);
+		i++;
 	}
+	tab[i].str = 0;
 	return (tab);
 }
