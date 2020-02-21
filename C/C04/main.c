c4de76e8e9d88024544fdcd3c33f6063f9f42b31
jricafor <jricafor@e3r11p11.42.fr>
2020-02-21 11:41:37 +0100

    hello

--- C/C04/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%d\n", ft_atoi("   123   +-5151qwfei"));
-}
--- Shell/shell01_2/ex04/MAC.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ifconfig -a | grep ether | cat -t | sed "s/\^Iether//g" | sed "s/ //g" 
