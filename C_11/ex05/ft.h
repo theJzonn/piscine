7236e7d667e142d3a82d78d8894bc46eb1a9f35b
theJzonn <jzonnmail@gmail.com>
2020-03-15 14:39:06 +0000

    ft.h

---
+++ C_11/ex05/ft.h
@@ -0,0 +1,12 @@
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+void	ft_do_op(char *nb1, char *nb2, char op);
+
+#endif
--- C_11/ex05/main.c
+++ C_11/ex05/main.c
@@ -10,18 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putnbr(int nb);
-void	ft_do_op(char *nb1, char *nb2, char op);
+#include "ft.h"
 
 int		ft_strlen(char *str)
 {
 	int i;
-	int res;
 
 	i = 0;
-	res = 0;
 	while (str[i] != '\0')
 		i++;
 	return (i);
